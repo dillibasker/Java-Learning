43f7ca06489e1eb7ef7fe5d75c36b388cb6c359e
Dilli Basker <dillibasker1@gmail.com>
2026-02-02 20:58:46 +0530

    fix

--- src/main/java/demo/helloworld/API_controller.java
+++ src/main/java/demo/helloworld/API_controller.java
@@ -1,16 +1,36 @@
 package demo.helloworld;
 
 import jakarta.websocket.server.PathParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/todo")
 public class API_controller {
+    //pathvariable
     @GetMapping("/{id}")
     String Crateuserid(@PathVariable long id){
-        return "id"+id;
+        return "pathvariable id is"+id;
+    }
+    //requestparamtodo?todoid=123
+    @GetMapping
+    String userid(@RequestParam("todoid") long id){
+        return "Requestparam id is "+id;
+    }
+
+    //requestparam
+    @GetMapping("/create")
+    String useid(@RequestParam String userId , @RequestParam String password){
+        return "Requestparam name is "+ userId +" " + "passord is " + password;
+    }
+
+    //requestparam
+    @GetMapping("/create")
+    String usid(@RequestParam String userId , @RequestParam String password){
+        return "Requestparam name is "+ userId +" " + "passord is " + password;
+    }
+
+    @PostMapping("/create")
+    String reuestbody(@RequestBody String body){
+        return body;
     }
 }
